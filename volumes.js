857fbfe9d1b388bf50092be838b154b53df49710
Sujay Polineni <sujayp.1207@gmail.com>
2022-07-20 23:07:04 -0400

    Updates

--- volumes.js
+++ volumes.js
@@ -12,21 +12,31 @@ let volumes = [
     id: 1,
     title: "SCIENTIFIC TERRAPIN VOL1 ISS1",
     filepath: __dirname + "/volumes/SCIENTIFIC TERRAPIN VOL1 ISS1.pdf",
+    cover: __dirname + "/covers/SCIENTIFIC TERRAPIN VOL1 ISS1.pdf",
   },
   {
     id: 2,
     title: "SCIENTIFIC TERRAPIN VOL2 ISS1",
     filepath: __dirname + "/volumes/SCIENTIFIC TERRAPIN VOL2 ISS1.pdf",
+    cover: __dirname + "/covers/SCIENTIFIC TERRAPIN VOL2 ISS1.pdf",
   },
   {
     id: 3,
-    title: "SCIENTIFIC TERRAPIN VOL3 ISS1",
-    filepath: __dirname + "/volumes/SCIENTIFIC TERRAPIN VOL3 ISS1.pdf",
+    title: "SCIENTIFIC TERRAPIN VOL2 ISS2",
+    filepath: __dirname + "/volumes/SCIENTIFIC TERRAPIN VOL2 ISS2.pdf",
+    cover: __dirname + "/covers/SCIENTIFIC TERRAPIN VOL2 ISS2.pdf",
   },
   {
     id: 4,
+    title: "SCIENTIFIC TERRAPIN VOL3 ISS1",
+    filepath: __dirname + "/volumes/SCIENTIFIC TERRAPIN VOL3 ISS1.pdf",
+    cover: __dirname + "/covers/SCIENTIFIC TERRAPIN VOL3 ISS1.pdf",
+  },
+  {
+    id: 5,
     title: "SCIENTIFIC TERRAPIN VOL3 ISS2",
     filepath: __dirname + "/volumes/SCIENTIFIC TERRAPIN VOL3 ISS2.pdf",
+    cover: __dirname + "/covers/SCIENTIFIC TERRAPIN VOL3 ISS2.pdf",
   },
 ];
 
